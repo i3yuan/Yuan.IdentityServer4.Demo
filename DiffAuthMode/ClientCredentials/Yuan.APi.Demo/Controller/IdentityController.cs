598e26cb6d0aa14b5bf223180768497bb9ac31ae
i3yuan <CevilleXu@qq.com>
2020-10-27 22:08:29 +0800

    ClientCredentials

---
+++ DiffAuthMode/ClientCredentials/Yuan.APi.Demo/Controller/IdentityController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yuan.APi.Demo.Controller
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class IdentityController:ControllerBase
+    {
+        [HttpGet("getUserClaims")]
+        [Authorize]
+        public IActionResult GetUserClaims()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
---
+++ DiffAuthMode/ClientCredentials/Yuan.IdentityServer4/Config.cs
@@ -0,0 +1,50 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yuan.IdentityServer4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("client_scope1")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("api1","api1")
+                {
+                    Scopes={"client_scope1" }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "credentials_client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "client_scope1" }
+                },
+            };
+    }
+}
