bbf2ce07f7738e259caee8ff24c6f767c8990037
i3yuan <CevilleXu@qq.com>
2020-12-15 22:00:01 +0800

    HybridFlow

---
+++ DiffAuthMode/HybridFlowAuthentication/Yuan.IdentityServer4/Config.cs
@@ -0,0 +1,75 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Yuan.IdentityServer4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("hybrid_scope1")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+
+                new ApiResource("api1","api1")
+                {
+                    Scopes={ "hybrid_scope1" },
+                    UserClaims={JwtClaimTypes.Role},  //添加Cliam 角色类型
+                    ApiSecrets={new Secret("apipwd".Sha256())}
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+
+
+                 new Client
+                {
+                    ClientId = "hybrid_client",
+                    ClientName = "hybrid Auth", 
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+
+                    RequirePkce = false,//v4.x需要配置这个
+
+                    RedirectUris ={
+                    "http://localhost:5002/signin-oidc", //跳转登录到的客户端的地址
+                    },
+                    // RedirectUris = {"http://localhost:5002/auth.html" }, //跳转登出到的客户端的地址
+                    PostLogoutRedirectUris ={
+                        "http://localhost:5002/signout-callback-oidc",
+                    },
+                   
+                    AllowedScopes = {
+                           IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile,
+                         "hybrid_scope1"
+                     },
+                      //允许将token通过浏览器传递
+                    // AllowAccessTokensViaBrowser=true,
+                    // AllowOfflineAccess=true,
+                     // 是否需要同意授权 （默认是false）
+                      RequireConsent=true
+                 }
+            };
+    }
+}
---
+++ DiffAuthMode/HybridFlowAuthentication/Yuan.MVC.Demo/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Yuan.MVC.Demo.Models;
+
+namespace Yuan.MVC.Demo.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            ViewData["Message"] = "Secure page.";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 测试请求API资源(api1)
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> getApi()
+        {
+            var client = new HttpClient();
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                return Json(new { msg = "accesstoken 获取失败" });
+            }
+
+            // client.SetBearerToken(accesstoken);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            //var httpResponse = await client.GetAsync("http://localhost:5003/api/identity/GetUserClaims");
+            //var result = await httpResponse.Content.ReadAsStringAsync();
+            //if (!httpResponse.IsSuccessStatusCode)
+            //{
+            //    return Json(new { msg = "请求 api1 失败。", error = result });
+            //}
+            //return Json(new
+            //{
+            //    msg = "成功",
+            //    data = JsonConvert.DeserializeObject(result)
+            //});
+            var content = await client.GetStringAsync("http://localhost:5003/api/identity/GetUserClaims");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("json");
+        }
+    }
+}
